d58f459f9f2b91871f0dbf6ff64f835667af2286
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-01-12 02:22:45 +0530

    react states

--- src/index.js
+++ src/index.js
@@ -15,16 +15,41 @@ import ReactDOM from 'react-dom';
 // }
 
 class App extends React.Component{
-    render(){
+
+    constructor(props) {
+        super(props)
+        this.state = { latitude: null,errorMessage:'' }
+
+
         window.navigator.geolocation.getCurrentPosition(
-            (position)=>console.log(position),
-            (error) => console.log(error)
+            (position)=> {
+                this.setState({latitude: position.coords.latitude})
+            },
+            (error) => {
+                this.setState({errorMessage :error.message})
+            }
         );
-        return(
-            <div>
-                <h3>You are in the nothern hemisphere</h3>
-            </div>
-        )
+    }
+    render(){
+      
+        //return(
+            // <div>
+            //     <h3>{this.state.latitude}</h3>
+            //     <h3>{this.state.errorMessage}</h3>
+            // </div>
+            //)
+            if(this.state.errorMessage && !this.state.latitude){
+                return <div>{this.state.errorMessage}</div>
+
+            }
+            if(!this.state.errorMessage && this.state.latitude){
+                return<div>{this.state.latitude}</div>
+
+            }
+            else{
+                return <div>Loading ...</div>
+            }
+        
     }
 }
 
