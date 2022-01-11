38dd7462f102ead9e8005ab8d6f3a951d5832b83
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-01-11 23:45:48 +0530

    bootstrap

---
+++ src/HemisphereDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HemisphereDisplay = () =>{
+    return(
+        <div>
+            Hey You are in Nothern Hemisphere !
+        </div>
+    )
+}
+
+export default HemisphereDisplay;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App =()=>{
-    return(
-        <div>
-            
-            Hello World !
-        </div>
-    )
+// const App = () => {
+
+    
+//     return(
+//         <div>
+//            <h1>Latitude:</h1> 
+//         </div>
+//     )
+// }
+
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>console.log(position),
+            (error) => console.log(error)
+        );
+        return(
+            <div>
+                <h3>You are in the nothern hemisphere</h3>
+            </div>
+        )
+    }
 }
+
+
+
+
 
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
+
 )
+
+
+
+
